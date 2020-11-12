bc86c3545cb544b9009b4dfbfa8e38dbfaa9b2a2
sachingowdaac <58177201+sachingowdaac@users.noreply.github.com>
2020-11-13 00:08:03 +0530

    Navbar with search input

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="bg-purple-500 p-2 text-center text-purple-800 shadow">
+      <p>Copyright &copy; 2020</p>
+      <p>Created by Sachin with love</p>
+    </div>
+  );
+};
+
+export default Footer;
