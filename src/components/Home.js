efca6c8fab2c77bd9fbf75c427b55ae5aefaadd4
sachingowdaac <58177201+sachingowdaac@users.noreply.github.com>
2020-11-16 18:56:38 +0530

    Removed white spaces and comment line

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,15 @@
 import React from 'react';
-//import Books from '../books.json';
+
 
 const Home = ({ books }) => {
   return (
-    <div className=" flex flex-wrap p-5 justify-center ">
+    <div className="flex flex-wrap p-5 justify-center">
       {books.map((book, index) => {
         const { author, country, imageLink, pages, link, title, year } = book;
         return (
           <div
             key={index}
-            className="  max-w-sm m-3 bg-orange-600 rounded-lg shadow-xl overflow-hidden "
+            className="max-w-sm m-3 bg-orange-600 rounded-lg shadow-xl overflow-hidden"
           >
             <a href={link} rel="noreferrer" target="_blank">
               <div>
@@ -20,13 +20,13 @@ const Home = ({ books }) => {
                 />
               </div>
               <div className="p-2">
-                <h1 className=" text-pink-200 text-xl font-serif">
+                <h1 className="text-pink-200 text-xl font-serif">
                   Author: {author}
                 </h1>
-                <p className=" font-bold text-purple-900 font-normal italic text-xl">
+                <p className="font-bold text-purple-900 font-normal italic text-xl">
                   Titel: {title}
                 </p>
-                <div className="  font-hairline font-black inline-block">
+                <div className="font-hairline font-black inline-block">
                   <span>Year-{year}</span>
                   <span>Country-{country}</span>
                   <span>Pages-{pages}</span>
